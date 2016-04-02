4b7119e0305c29021e5639c7d8c24e70e90bc0e5
cristiancvanega <cristian1853@gmail.com>
2016-04-02 14:11:55 -0500

    Initial Commit

---
+++ MyLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLocation
+//
+//  Created by Cristian Castro on 4/2/16.
+//  Copyright © 2016 Cristian Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
